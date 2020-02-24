507072fe15b5dd2f5273970bbd379f579df4628d
Orlando Ramos <orlandxrf@gmail.com>
2020-02-24 16:19:22 -0600

    script para obtener oraciones de los documentos

---
+++ makeSentencesCorpus.py
@@ -0,0 +1,74 @@
+import sys
+
+def saveIntoFile(fname, data, mode='a'):
+	"""Save your data into file"""
+	g = open(fname, mode)
+	g.write(data)
+	g.close()
+
+def splitSpanishSentences(text):
+	"""
+	Requirements
+	pip install -U nltk
+	nltk.download('punkt')
+	-------------------------------
+	return sentences list of the text
+	"""
+	import nltk
+	spanish_tokenizer = nltk.data.load('tokenizers/punkt/PY3/spanish.pickle')
+	sentences = spanish_tokenizer.tokenize(text)
+	return sentences
+
+def cleanText(text):
+    """Preproccess document and clean"""
+    from nltk.corpus import stopwords
+    import re
+
+    text = text.lower() # lowercase text
+    text = re.sub('[^a-zñáéíóöúü ]', ' ', text)
+    text = ' '.join(text.split())
+    text = ' '.join(word for word in text.split() if word not in set(stopwords.words('spanish')) ) # remove stopwords
+    return text
+
+categories = {
+	'crimen': 'crimen.txt',
+	'cultura': 'cultura.txt',
+	'deportes': 'deportes.txt',
+	'economia': 'economia.txt',
+	'espectaculos': 'espectaculos.txt',
+	'finanzas': 'finanzas.txt',
+	'gossip': 'gossip.txt',
+	'justicia': 'justicia.txt',
+	'policia': 'policia.txt',
+	'politica': 'politica.txt',
+	'salud': 'salud.txt',
+	'seguridad': 'seguridad.txt',
+	'show': 'show.txt',
+	'sociedad': 'sociedad.txt',
+	'tecnologia': 'tecnologia.txt',
+}
+
+
+fnsave = 'data/sentences_corpora.txt'
+data = 'category\tid_doc\tsentence\n'
+saveIntoFile(fnsave, data, 'w')
+
+
+for cat in categories:
+	# filename = 'data/categories/corpus/{}'.format( categories[cat] )
+	filename = 'data/corpus/{}'.format( categories[cat] )
+	with open(filename, 'r') as f:
+		for i, row in enumerate(f):
+			if i==0: continue # skip headers
+			sys.stdout.write( '\tProcesando {} documentos...\r'.format( format(i), ',d') )
+			sys.stdout.flush()
+			category, doc = row.replace('\n','').split('\t')
+			sentences = splitSpanishSentences(doc)
+			for snt in sentences:
+				snt = cleanText(snt)
+				data = '{}\t{}\t{}\n'.format(cat, i, snt)
+				saveIntoFile(fnsave, data)
+			break
+	break
+	f.close()
+	print ('\n\tTerminado!\n\n')
