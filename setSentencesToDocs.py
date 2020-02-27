db3f4d3366b539ab864817dbea23dd798a2c067f
Orlando Ramos <orlandxrf@gmail.com>
2020-02-27 17:45:48 -0600

    correción, se agregaron las categorías así como el id del documento

--- setSentencesToDocs.py
+++ setSentencesToDocs.py
@@ -8,25 +8,28 @@ def saveIntoFile(fname, data, mode='a'):
 
 filename = 'data/sentences_ok.txt'
 fnsave = 'data/corpora_ok.txt'
-data = 'id_doc\tdoc\n'
+data = 'category\tid_doc\tdoc\n'
 saveIntoFile(fnsave, data, 'w')
 
 tmp_id = 0
+tmp_category = ""
 document = []
 with open(filename, 'r') as f:
 	for i, row in enumerate(f):
 		if i==0: continue # skip headers
 		sys.stdout.write( '\tProcesando {} oraciones...\r'.format( format(i,',d') ) )
 		sys.stdout.flush()
-		catgeory, id_doc, sentence = row.replace('\n','').split('\t')
+		category, id_doc, sentence = row.replace('\n','').split('\t')
+		if tmp_category == "": tmp_category = category
 		if tmp_id == 0: tmp_id = id_doc
 		if id_doc != tmp_id:
-			data = '{}\t{}\n'.format(tmp_id, ' '.join(document))
+			data = '{}\t{}\t{}\n'.format(tmp_category, tmp_id, ' '.join(document))
 			saveIntoFile(fnsave, data)
 			tmp_id = id_doc
+			tmp_category = category
 			document = []
 		else: document += sentence.split()
 f.close()
-data = '{}\t{}\n'.format(tmp_id, ' '.join(document))
+data = '{}\t{}\t{}\n'.format(tmp_category, tmp_id, ' '.join(document))
 saveIntoFile(fnsave, data)
 print ('\n\tTerminado!\n')
