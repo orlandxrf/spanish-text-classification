bffb4618bbbbee367b579d101ab5ccf559251619
Orlando Ramos <orlandxrf@gmail.com>
2020-03-06 14:51:15 -0600

    Change data to plot results

--- plotResults.py
+++ plotResults.py
@@ -1,114 +1,152 @@
-def plot3Bars(data):
+def plot3Bars(data, labels, metric):
 	import matplotlib.pyplot as plt
 	import numpy as np
 
 
-	index = np.arange(len(data))
+	index = np.arange(len( [data[0][cat][metric] for cat in data[0]] ))
 
 	plt.figure(figsize=(15,8))
 	ax = plt.subplot(111)
 
-	labels = ['original', 'total', 'duplicates']
+	# labels = ['LR', 'SVM', 'NB']
+	labels.reverse()
 
-	w = 0.3
-	for i, category in enumerate(data):
-		ax.bar(i-0.3, data[category]['original'], width=w, color='tab:green', align='center', label=labels[0])
-		ax.bar(i, data[category]['total'], width=w, color='tab:blue', align='center', label=labels[1])
-		ax.bar(i+0.3, data[category]['duplicates'], width=w, color='tab:red', align='center', label=labels[2])
-	plt.xticks(index, list(data.keys()), fontsize=12, rotation=30)
+	w = 0.2
+	for i, category in enumerate(data[0]):
+		ax.bar(i-(w/2+w), data[3][category][metric], width=w, color='tab:green', align='center', label=labels[3])
+		ax.bar(i-(w/2), data[2][category][metric], width=w, color='tab:orange', align='center', label=labels[2])
+		ax.bar(i+(w/2), data[1][category][metric], width=w, color='tab:red', align='center', label=labels[1])
+		ax.bar(i+(w/2+w), data[0][category][metric], width=w, color='tab:blue', align='center', label=labels[0])
+	plt.xticks(index, list(data[0].keys()), fontsize=12, rotation=30)
 	plt.yticks(fontsize=12)
-	plt.xlabel('Document categories', fontsize=14)
-	plt.title('Total sentences number by category', fontsize=14)
+	plt.xlabel('Categorías', fontsize=14)
+	plt.ylabel('{} score'.format(metric), fontsize=14)
+	# plt.title('Resultados de la métrica F1 sobre los clasificadores', fontsize=14)
 	plt.legend(labels=labels, loc='best', fontsize=12)
 	plt.tight_layout()
-	plt.grid()
-	plt.savefig('img/sentences.png')
+	# plt.grid()
+	plt.savefig( 'img/{}_score_bars.png'.format(metric) )
+	plt.savefig( 'img/{}_score_bars.eps'.format(metric) )
 	plt.show()
 
 
 
-
-def plotLines(data):
+def plotLines(data, labels, metric):
 	import matplotlib.pyplot as plt
 	import numpy as np
 
-	index = np.arange(len(data))
+	index = np.arange(len( [data[0][cat][metric] for cat in data[0]] ))
 
-	labels = ['original', 'duplicates', 'total']
+	# labels = ['NB', 'SVM', 'LR']
 
-	ori = [ data[item]['original'] for item in data]
-	dup = [ data[item]['duplicates'] for item in data]
-	tot = [ data[item]['total'] for item in data]
+	# ori = [ data[item]['precision'] for item in data]
+	# dup = [ data[item]['recall'] for item in data]
+	# tot = [ data[item]['f1-score'] for item in data]
 
 	plt.figure(figsize=(15,8))
-	plt.plot(index, ori, '-go')
-	plt.plot(index, dup, '-ro')
-	plt.plot(index, tot, '-bo')
-	plt.xticks(index, list(data.keys()), fontsize=12, rotation=30)
+	plt.plot(index, [data[0][cat][metric] for cat in data[0]], 'tab:blue', marker='o')
+	plt.plot(index, [data[1][cat][metric] for cat in data[1]], 'tab:red', marker='o')
+	plt.plot(index, [data[2][cat][metric] for cat in data[2]], 'tab:orange', marker='o')
+	plt.plot(index, [data[3][cat][metric] for cat in data[3]], 'tab:green', marker='o')
+	# plt.plot(index, dup, '-ro')
+	# plt.plot(index, tot, '-bo')
+	plt.xticks(index, list(data[0].keys()), fontsize=12, rotation=30)
 	plt.yticks(fontsize=12)
-	plt.xlabel('Document categories', fontsize=14)
-	plt.title('Total sentences number by category', fontsize=14)
+	plt.xlabel('Categorías', fontsize=14)
+	plt.ylabel('{} score'.format(metric), fontsize=14)
+	# plt.title('Total sentences number by category', fontsize=14)
 	plt.legend(labels=labels, loc='best', fontsize=12)
 	plt.tight_layout()
-	plt.grid()
-	# plt.savefig('img/categories.png')
+	# plt.grid()
+	plt.savefig( 'img/{}_score_lines.png'.format(metric) )
+	plt.savefig( 'img/{}_score_lines.eps'.format(metric) )
 	plt.show()
 
 
 
 naive = {
-	'crime': {'pr': 0.00, 're': 0.00, 'f1': 0.00},
-	'culture': {'pr': 0.84, 're': 0.69, 'f1': 0.76},
-	'economy': {'pr': 0.71, 're': 0.67, 'f1': 0.69},
-	'finance': {'pr': 1.00, 're': 0.05, 'f1': 0.09},
-	'gossip': {'pr': 0.00, 're': 0.00, 'f1': 0.00},
-	'health': {'pr': 0.88, 're': 0.58, 'f1': 0.70},
-	'justice': {'pr': 0.96, 're': 0.12, 'f1': 0.22},
-	'police': {'pr': 0.56, 're': 0.98, 'f1': 0.71},
-	'politics': {'pr': 0.54, 're': 0.86, 'f1': 0.67},
-	'security': {'pr': 0.76, 're': 0.08, 'f1': 0.14},
-	'show': {'pr': 0.94, 're': 0.20, 'f1': 0.33},
-	'society': {'pr': 0.66, 're': 0.28, 'f1': 0.39},
-	'spectacle': {'pr': 0.66, 're': 0.90, 'f1': 0.76},
-	'sports': {'pr': 0.80, 're': 0.96, 'f1': 0.88},
-	'technology': {'pr': 0.93, 're': 0.35, 'f1': 0.51},
+	'deportes': {'Precision': 0.80, 'Recall': 0.96, 'F1': 0.88},
+	'policiaca': {'Precision': 0.56, 'Recall': 0.98, 'F1': 0.71},
+	'espectáculos': {'Precision': 0.66, 'Recall': 0.90, 'F1': 0.76},
+	'política': {'Precision': 0.54, 'Recall': 0.86, 'F1': 0.67},
+	'seguridad': {'Precision': 0.76, 'Recall': 0.08, 'F1': 0.14},
+	'sociedad': {'Precision': 0.66, 'Recall': 0.28, 'F1': 0.39},
+	'cultura': {'Precision': 0.84, 'Recall': 0.69, 'F1': 0.76},
+	'economía': {'Precision': 0.71, 'Recall': 0.67, 'F1': 0.69},
+	'justicia': {'Precision': 0.96, 'Recall': 0.12, 'F1': 0.22},
+	'show': {'Precision': 0.94, 'Recall': 0.20, 'F1': 0.33},
+	'salud': {'Precision': 0.88, 'Recall': 0.58, 'F1': 0.70},
+	'tecnología': {'Precision': 0.93, 'Recall': 0.35, 'F1': 0.51},
+	'finanzas': {'Precision': 1.00, 'Recall': 0.05, 'F1': 0.09},
+	'gossip': {'Precision': 0.00, 'Recall': 0.00, 'F1': 0.00},
+	'crimen': {'Precision': 0.00, 'Recall': 0.00, 'F1': 0.00},
 }
 
 svm = {
-	'crime': {'pr': 0.94, 're': 0.11, 'f1': 0.19},
-	'culture': {'pr': 0.78, 're': 0.74, 'f1': 0.76},
-	'economy': {'pr': 0.67, 're': 0.78, 'f1': 0.72},
-	'finance': {'pr': 0.91, 're': 0.13, 'f1': 0.23},
-	'gossip': {'pr': 0.60, 're': 0.01, 'f1': 0.02},
-	'health': {'pr': 0.76, 're': 0.80, 'f1': 0.78},
-	'justice': {'pr': 0.66, 're': 0.22, 'f1': 0.33},
-	'police': {'pr': 0.64, 're': 0.96, 'f1': 0.77},
-	'politics': {'pr': 0.64, 're': 0.80, 'f1': 0.71},
-	'security': {'pr': 0.76, 're': 0.35, 'f1': 0.48},
-	'show': {'pr': 0.96, 're': 0.71, 'f1': 0.82},
-	'society': {'pr': 0.69, 're': 0.29, 'f1': 0.41},
-	'spectacle': {'pr': 0.79, 're': 0.87, 'f1': 0.83},
-	'sports': {'pr': 0.82, 're': 0.97, 'f1': 0.89},
-	'technology': {'pr': 0.80, 're': 0.58, 'f1': 0.67},
+	'deportes': {'Precision': 0.82, 'Recall': 0.97, 'F1': 0.89},
+	'policiaca': {'Precision': 0.64, 'Recall': 0.96, 'F1': 0.77},
+	'espectáculos': {'Precision': 0.79, 'Recall': 0.87, 'F1': 0.83},
+	'política': {'Precision': 0.64, 'Recall': 0.80, 'F1': 0.71},
+	'seguridad': {'Precision': 0.76, 'Recall': 0.35, 'F1': 0.48},
+	'sociedad': {'Precision': 0.69, 'Recall': 0.29, 'F1': 0.41},
+	'cultura': {'Precision': 0.78, 'Recall': 0.74, 'F1': 0.76},
+	'economía': {'Precision': 0.67, 'Recall': 0.78, 'F1': 0.72},
+	'justicia': {'Precision': 0.66, 'Recall': 0.22, 'F1': 0.33},
+	'show': {'Precision': 0.96, 'Recall': 0.71, 'F1': 0.82},
+	'salud': {'Precision': 0.76, 'Recall': 0.80, 'F1': 0.78},
+	'tecnología': {'Precision': 0.80, 'Recall': 0.58, 'F1': 0.67},
+	'finanzas': {'Precision': 0.91, 'Recall': 0.13, 'F1': 0.23},
+	'gossip': {'Precision': 0.60, 'Recall': 0.01, 'F1': 0.02},
+	'crimen': {'Precision': 0.94, 'Recall': 0.11, 'F1': 0.19},
 }
 
 
 lgr = {
-	'crime': {'pr': 0.95, 're': 0.92, 'f1': 0.94},
-	'culture': {'pr': 0.83, 're': 0.84, 'f1': 0.84},
-	'economy': {'pr': 0.83, 're': 0.83, 'f1': 0.83},
-	'finance': {'pr': 0.83, 're': 0.80, 'f1': 0.82},
-	'gossip': {'pr': 0.92, 're': 0.82, 'f1': 0.87},
-	'health': {'pr': 0.83, 're': 0.83, 'f1': 0.83},
-	'justice': {'pr': 0.67, 're': 0.65, 'f1': 0.66},
-	'police': {'pr': 0.92, 're': 0.94, 'f1': 0.93},
-	'politics': {'pr': 0.75, 're': 0.76, 'f1': 0.75},
-	'security': {'pr': 0.82, 're': 0.82, 'f1': 0.82},
-	'show': {'pr': 0.97, 're': 0.97, 'f1': 0.97},
-	'society': {'pr': 0.53, 're': 0.54, 'f1': 0.54},
-	'spectacle': {'pr': 0.93, 're': 0.95, 'f1': 0.94},
-	'sports': {'pr': 0.97, 're': 0.96, 'f1': 0.97},
-	'technology': {'pr': 0.84, 're': 0.82, 'f1': 0.83},
+	'deportes': {'Precision': 0.97, 'Recall': 0.96, 'F1': 0.97},
+	'policiaca': {'Precision': 0.92, 'Recall': 0.94, 'F1': 0.93},
+	'espectáculos': {'Precision': 0.93, 'Recall': 0.95, 'F1': 0.94},
+	'política': {'Precision': 0.75, 'Recall': 0.76, 'F1': 0.75},
+	'seguridad': {'Precision': 0.82, 'Recall': 0.82, 'F1': 0.82},
+	'sociedad': {'Precision': 0.53, 'Recall': 0.54, 'F1': 0.54},
+	'cultura': {'Precision': 0.83, 'Recall': 0.84, 'F1': 0.84},
+	'economía': {'Precision': 0.83, 'Recall': 0.83, 'F1': 0.83},
+	'justicia': {'Precision': 0.67, 'Recall': 0.65, 'F1': 0.66},
+	'show': {'Precision': 0.97, 'Recall': 0.97, 'F1': 0.97},
+	'salud': {'Precision': 0.83, 'Recall': 0.83, 'F1': 0.83},
+	'tecnología': {'Precision': 0.84, 'Recall': 0.82, 'F1': 0.83},
+	'finanzas': {'Precision': 0.83, 'Recall': 0.80, 'F1': 0.82},
+	'gossip': {'Precision': 0.92, 'Recall': 0.82, 'F1': 0.87},
+	'crimen': {'Precision': 0.95, 'Recall': 0.92, 'F1': 0.94},
 }
 
 
+lstm = {
+	'deportes': {'Precision': 0.98, 'Recall': 0.94, 'F1': 0.96},
+	'policiaca': {'Precision': 0.92, 'Recall': 0.94, 'F1': 0.93},
+	'espectáculos': {'Precision': 0.95, 'Recall': 0.93, 'F1': 0.94},
+	'política': {'Precision': 0.74, 'Recall': 0.81, 'F1': 0.77},
+	'seguridad': {'Precision': 0.84, 'Recall': 0.83, 'F1': 0.84},
+	'sociedad': {'Precision': 0.61, 'Recall': 0.58, 'F1': 0.60},
+	'cultura': {'Precision': 0.78, 'Recall': 0.83, 'F1': 0.81},
+	'economía': {'Precision': 0.80, 'Recall': 0.86, 'F1': 0.83},
+	'justicia': {'Precision': 0.71, 'Recall': 0.69, 'F1': 0.70},
+	'show': {'Precision': 0.91, 'Recall': 0.97, 'F1': 0.94},
+	'salud': {'Precision': 0.87, 'Recall': 0.79, 'F1': 0.83},
+	'tecnología': {'Precision': 0.84, 'Recall': 0.82, 'F1': 0.83},
+	'finanzas': {'Precision': 0.78, 'Recall': 0.81, 'F1': 0.79},
+	'gossip': {'Precision': 0.84, 'Recall': 0.78, 'F1': 0.81},
+	'crimen': {'Precision': 0.95, 'Recall': 0.91, 'F1': 0.93},
+}
+
+
+#     accuracy                           0.86     90035
+#    macro avg       0.83      0.83      0.83     90035
+# weighted avg       0.87      0.86      0.87     90035
+
+
+
+
+labels = ['NB', 'SVM', 'LSTM', 'LR']
+
+plotLines( (naive, svm, lstm, lgr), labels, 'Recall' )
+plot3Bars( (naive, svm, lstm, lgr), labels, 'Recall' )
